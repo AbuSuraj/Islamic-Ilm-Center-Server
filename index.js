448789fe8c34cb3437eaaf3ecbe07faeb5dacfad
abu bokor suraj <abusurajbd@gmail.com>
2022-10-25 03:09:09 +0600

    categories json folder  created

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
+const cors = require('cors');
+app.use(cors());
+
+const categories = require('./data/course-category.json');
+
+const course = require('./data/course-details.json')
 
 app.get('/', (req, res) => {
   res.send('Ilm Center api is running!')
 })
+app.get('/categories', (req, res) => {
+  res.send(categories)
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
