f4757a73dfb350ab68f8cba118f9f3e8d0d40b08
abu bokor suraj <abusurajbd@gmail.com>
2022-10-25 03:12:15 +0600

    courses json folder  created

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000;
-const cors = require('cors');
-app.use(cors());
+
 
 const categories = require('./data/course-category.json');
 
